5ffb7aea319d158170f71a09bc0bd1e1debdbb26
RedBlaze908 <redblaze212@gmail.com>
2025-09-05 18:23:18 +0200

    Add project files.

---
+++ Readme.md
@@ -0,0 +1,9 @@
+## Template For C++ Games
+This is a template for creating 2D games with C++ with Cmake.
+Feel free to use this repository for your projects!
+
+This repository was inspired (and i also taken some piece of code from: https://github.com/chfhhd/raylibstarter-minimal)
+
+## Library Included
+	- Raylib (Version: 4.5.0) (Window Rendering, sprites ecc..)
+	- Enet (Version: master) (Client & Server, Multiplayer)
---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+﻿#include <raylib.h>
+#include "config.h"
+
+
+int main() {
+    InitWindow(Game::ScreenWidth, Game::ScreenHeight, Game::PROJECT_NAME);
+    SetTargetFPS(60);
+
+
+    Texture2D myTexture = LoadTexture("assets/graphics/testimage.png");
+
+    while (!WindowShouldClose()) {
+
+        BeginDrawing();
+        ClearBackground(WHITE);
+        DrawText("Hello World", 20, 20, 20, RED);
+        DrawTexture(myTexture, 10, 100, WHITE);
+
+        EndDrawing();
+    }
+
+    UnloadTexture(myTexture);
+
+    CloseWindow();
+
+    return 1;
+}
